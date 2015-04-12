eeba31f202f95d86c269642c8e63087d6ba60843
petruz <info@petruz.it>
2015-04-12 23:12:16 +0200

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,12 @@
+# read data from file
+data<- read.table("data/household_power_consumption.txt", sep=";", header=T, quote= "", strip.white=TRUE, stringsAsFactors = F, na.strings=c("NA", "-", "?"))
+
+#  gert only the two days: 
+data<- subset(data, (data$Date == "1/2/2007" | data$Date== "2/2/2007")) 
+
+# creating Plot1
+png("plot1.png", width=480, height= 480)
+
+hist(data$Global_active_power, col= "red", xlab= "Global Active Power (kilowatts)", ylab= "Frequency", main= "Global Active Power")
+
+
