e21caa01c7d1353ba84245c0a2ef206f5e6dc905
petruz <info@petruz.it>
2015-04-12 23:25:11 +0200

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,29 @@
+# read data from file
+data<- read.table("data/household_power_consumption.txt", sep=";", header=T, quote= "", strip.white=TRUE, stringsAsFactors = F, na.strings=c("NA", "-", "?"))
+
+#  gert only the two days: 
+data<- subset(data, (data$Date == "1/2/2007" | data$Date== "2/2/2007")) 
+
+# correct date format
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
+
+# creating Plot4
+png("plot4.png", width = 480, height = 480)
+
+# four subplots
+par(mfrow=c(2,2)) 
+# top left (1)
+plot(data$DateTime, data$Global_active_power, type= "l", lwd=1, ylab= "Global Active Power (kilowatts)", xlab="") 
+# top right (2)
+plot(data$DateTime, data$Voltage, type="l", xlab="datetime", ylab="Voltage")   
+# bottom left (3)
+plot(data$DateTime, data$Sub_metering_1, type="l", ylab= "Energy sub metering", xlab="")
+lines(data$DateTime, data$Sub_metering_2, type="l", col="red")
+lines(data$DateTime, data$Sub_metering_3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, bty="n", col=c("black", "red", "blue")) 
+# bottom right (4)
+plot(data$DateTime, data$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")  
+
+dev.off()
